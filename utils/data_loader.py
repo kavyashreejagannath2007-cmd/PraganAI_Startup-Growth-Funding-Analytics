42b4baec3b204ef9293a0a7b4885f003a58e646c
kavyashreejagannath2007-cmd <kavyashreejagannath2007@gmail.com>
2026-06-04 15:58:15 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,36 +1,10 @@
+from pathlib import Path
 import pandas as pd
 import streamlit as st
 
-
 @st.cache_data
 def load_data():
-    """
-    Load the startup dataset.
-    """
-    df = pd.read_csv("data/startup_data.csv")
-    return df
+    base_dir = Path(__file__).resolve().parent.parent
+    csv_path = base_dir / "data" / "startup_data.csv"
 
-
-@st.cache_data
-def load_filtered_data(industry=None, region=None):
-
-    df = load_data()
-
-    if industry:
-        df = df[df["Industry"].isin(industry)]
-
-    if region:
-        df = df[df["Region"].isin(region)]
-
-    return df
-
-
-def get_numeric_columns():
-    return [
-        "Funding Rounds",
-        "Funding Amount (M USD)",
-        "Valuation (M USD)",
-        "Revenue (M USD)",
-        "Employees",
-        "Market Share (%)"
-    ]
+    return pd.read_csv(csv_path)
